9f9e36ce94918bf2a0c73abd354e39326e39569d
Lukasz Fundakowski <lukasz.fundakowski@nordicsemi.no>
2024-11-19 15:37:56 +0100

    scripts/bootloader: Add ed25519/sha512 to scripts
    Python scripts implementing ed25519 and sha512 support needed
    for nsib image signing.
    
    Signed-off-by: Lukasz Fundakowski <lukasz.fundakowski@nordicsemi.no>

---
+++ scripts/bootloader/tests/conftest.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2024 Nordic Semiconductor ASA
+#
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
