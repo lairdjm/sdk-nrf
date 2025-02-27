7aad34a9eee8405cc96cb05d2fe26572ac430517
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2025-02-27 08:54:58 +0000

    todo

---
+++ samples/liteupdateapp/src/main.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/logging/log.h>
+#include <bootutil/image.h>
+
+LOG_MODULE_REGISTER(todo, LOG_LEVEL_DBG);
+
+/*
+uint8_t image_count;
+
+off_t start_address;
+size_t image_size;
+*/
+
+int main(void)
+{
+	LOG_ERR("todo");
+	return 0;
+}
